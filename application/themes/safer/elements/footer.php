95d71729e8a9edd1ebe5267cd96829db979fcbb8
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-10-01 01:38:51 -0400

    Setup newsletter

--- application/themes/safer/elements/footer.php
+++ application/themes/safer/elements/footer.php
@@ -21,17 +21,21 @@
             </div>
             <div class="col-md-5">
               <p><b>Join Our Newsletter!</b></p>
-              <form class="form-horizontal" role="form">
+              <form class="form-horizontal" role="form" action="http://www.trailblz.info/SAFER/signup_post.aspx" method="get" name="newsLetterForm" onsubmit="return validateform()">
+                <input type="hidden" name="rd" value="http://safercampus.org">
                 <div class="form-group">
                   <div class="col-sm-12">
-                    <input type="email" class="form-control" id="" placeholder="Email">
+                    <input type="email" name="email" class="form-control" id="" placeholder="Email">
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" id="" placeholder="Name">
+                  <div class="col-sm-4">
+                    <input name="firstname" type="text" class="form-control" id="" placeholder="First Name">
                   </div>
-                  <div class="col-sm-3">
+                  <div class="col-sm-4">
+                    <input name="lastname" type="text" class="form-control" id="" placeholder="Last Name">
+                  </div>
+                  <div class="col-sm-2">
                     <button type="submit" class="btn btn-primary">Subscribe</button>
                   </div>
                 </div>
@@ -45,4 +49,36 @@
   </div>
   <? Loader::element('footer_required'); // concrete5 setup  ?>
 </body>
+<script type="text/javascript">
+  // http://stackoverflow.com/a/2855946
+  function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+  };
+
+  function validateform() {
+    var form = $("form[name='newsLetterForm']");
+    var firstname = form.find("input[name='firstname']").val();
+    var lastname = form.find("input[name='lastname']").val();
+    var email = form.find("input[name='email']").val();
+    var emailGroup = $("form").find(".form-group").first();
+    var nameGroup = $("form").find(".form-group").last();
+
+    var ret = true;
+    if (firstname == "" || lastname == "") {
+      nameGroup.addClass("has-error");
+      ret = false;
+    } else {
+      nameGroup.removeClass("has-error");
+    }
+    if (!isValidEmailAddress(email)) {
+      emailGroup.addClass("has-error");
+      ret = false;
+    } else {
+      emailGroup.removeClass("has-error");
+    }
+
+    return ret;
+  }
+</script>
 </html>

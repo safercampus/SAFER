3eca9864179e027c04b34c2703a72dd4a9c8204d
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-09-30 20:38:57 -0400

    Add proper logo and favicons
    Used an awesome online tool to generate all of the icon files and the favicon code - http://realfavicongenerator.net/

--- application/themes/safer/elements/header.php
+++ application/themes/safer/elements/header.php
@@ -2,6 +2,24 @@
 
 <head>
   <?=Loader::element('header_required'); // concrete5 setup ?>
+  <? /* Favicon code and images generated with http://realfavicongenerator.net/ */ ?>
+  <link rel="apple-touch-icon" sizes="57x57" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?=$view->getThemePath()?>/img/favicons/apple-touch-icon-180x180.png">
+  <link rel="shortcut icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="shortcut icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="shortcut icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+  <link rel="shortcut icon" type="image/png" href="<?=$view->getThemePath()?>/img/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?=$view->getThemePath()?>/img/favicons/mstile-144x144.png">
+  <meta name="theme-color" content="#ffffff">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -29,7 +47,7 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="/">
-            <img alt="Draft pennant logo blue" class="logo" src="<?=$view->getThemePath()?>/img/draft-pennant-logo-blue.png"> SAFER
+            <img alt="SAFER logo" class="logo" src="<?=$view->getThemePath()?>/img/logo.png">
           </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">

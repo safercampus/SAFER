39125a48f7807dc76392f7cdfcbb6a5270926fa6
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-10-01 20:42:39 -0400

    Animate bootstrap hamburger menu from three bars to an X
    See http://julienmelissas.com/animated-x-icon-for-the-bootstrap-navbar-toggle/ for details

--- application/themes/safer/elements/header.php
+++ application/themes/safer/elements/header.php
@@ -40,11 +40,16 @@
     <div class="navbar navbar-default" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <button type="button"
+              class="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#navbar"
+              aria-expanded="false"
+              aria-controls="#navbar">
             <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
           </button>
           <a class="navbar-brand" href="/">
             <img alt="SAFER logo" class="logo" src="<?=$view->getThemePath()?>/img/SAFER bullhorn logo.png">

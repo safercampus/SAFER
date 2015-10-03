e3dfebb8f52f1a77ae6d99599bbc4a42d755f930
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-10-03 11:10:13 -0400

    Changes to homepage and fix up page-logo css

--- application/themes/safer/home.php
+++ application/themes/safer/home.php
@@ -23,30 +23,56 @@
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4 main-sections">
-          <img class="page-logo" alt="The Basics" src="<?=$view->getThemePath()?>/img/The Basics icon.png">
+          <a href="/basics">
+            <img class="page-logo" alt="The Basics" src="<?=$view->getThemePath()?>/img/The Basics icon.png">
+          </a>
           <?php
               $a = new Area('Homepage The Basics Blurb');
               $a->display($c);
           ?>
-          <br>
-          <p><a class="btn btn-primary" href="basics" role="button">Learn more</a></p>
+          <div class="button-container">
+            <a class="btn btn-primary" href="/basics" role="button">
+              <?php
+                  $a = new Area('Homepage Basics Button');
+                  $a->display($c);
+              ?>
+            </a>
+          </div>
         </div>
+
         <div class="col-md-4 main-sections">
+              <a href="/policy">Policy
           <img class="page-logo" alt="Policy" src="<?=$view->getThemePath()?>/img/Policy icon.png">
           <?php
               $a = new Area('Homepage Policy Blurb');
               $a->display($c);
           ?>
-          <br>
-          <p><a class="btn btn-primary" href="policy" role="button">Learn more</a></p>
+          <div class="button-container">
+            <a class="btn btn-primary" href="/policy" role="button">
+              <?php
+                  $a = new Area('Homepage Policy Button');
+                  $a->display($c);
+              ?>
+            </a>
+          </div>
        </div>
+
         <div class="col-md-4 main-sections">
-          <img class="page-logo" alt="Activism" src="<?=$view->getThemePath()?>/img/Activism icon.png">
+          <a href="/activism">
+            <img class="page-logo" alt="Activism" src="<?=$view->getThemePath()?>/img/Activism icon.png">
+          </a>
           <?php
               $a = new Area('Homepage Activism Blurb');
               $a->display($c);
           ?>
-          <p><a class="btn btn-primary" href="activism" role="button">Learn more</a></p>
+          <div class="button-container">
+            <a class="btn btn-primary" href="/activism" role="button">
+              <?php
+                  $a = new Area('Homepage Activism Button');
+                  $a->display($c);
+              ?>
+            </a>
+          </div>
         </div>
       </div>
     </div>

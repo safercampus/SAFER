23c7a31edeb2cef8b956f65619995bf951ac359e
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-10-03 20:31:50 -0400

    Stick footer to bottom of page
    This fixes a problem where the footer would be up next to the page content which doesn't look good on pages with little content.

--- application/themes/safer/elements/footer.php
+++ application/themes/safer/elements/footer.php
@@ -1,51 +1,51 @@
-    <div>
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-3">
-              <div class="heading">School Sexual Assault</div>
-              <a href="/basics">The Basics</a><br>
-              <a href="/policy">Policy</a><br>
-              <a href="/activism">Activism</a>
-            </div>
-            <div class="col-sm-2">
-              <div class="heading">About Us</div>
-              <a href="/about">About</a><br>
-              <a href="/press">Press</a><br>
-            </div>
-            <div class="col-sm-2">
-              <div class="heading">Community</div>
-              <a href="https://twitter.com/safercampus">Twitter</a><br>
-              <a href="https://www.facebook.com/safercampus">Facebook</a><br>
-              <a href="/contact">Contact Us</a>
-            </div>
-            <div class="col-sm-5">
-              <div class="heading">Join Our Newsletter!</div>
-              <form class="form-horizontal" role="form" action="http://www.trailblz.info/SAFER/signup_post.aspx" method="get" name="newsLetterForm" onsubmit="return validateform()">
-                <input type="hidden" name="rd" value="http://safercampus.org/newsletter-thank-you">
-                <div class="form-group">
-                  <div class="col-md-12">
-                    <input type="email" name="email" class="form-control" id="" placeholder="Email">
-                  </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="heading">School Sexual Assault</div>
+            <a href="/basics">The Basics</a><br>
+            <a href="/policy">Policy</a><br>
+            <a href="/activism">Activism</a>
+          </div>
+          <div class="col-sm-2">
+            <div class="heading">About Us</div>
+            <a href="/about">About</a><br>
+            <a href="/press">Press</a><br>
+          </div>
+          <div class="col-sm-2">
+            <div class="heading">Community</div>
+            <a href="https://twitter.com/safercampus">Twitter</a><br>
+            <a href="https://www.facebook.com/safercampus">Facebook</a><br>
+            <a href="/contact">Contact Us</a>
+          </div>
+          <div class="col-sm-5">
+            <div class="heading">Join Our Newsletter!</div>
+            <form class="form-horizontal" role="form" action="http://www.trailblz.info/SAFER/signup_post.aspx" method="get" name="newsLetterForm" onsubmit="return validateform()">
+              <input type="hidden" name="rd" value="http://safercampus.org/newsletter-thank-you">
+              <div class="form-group">
+                <div class="col-md-12">
+                  <input type="email" name="email" class="form-control" id="" placeholder="Email">
                 </div>
-                <div class="form-group">
-                  <div class="col-md-4">
-                    <input name="firstname" type="text" class="form-control" id="" placeholder="First Name">
-                  </div>
-                  <div class="col-md-4">
-                    <input name="lastname" type="text" class="form-control" id="" placeholder="Last Name">
-                  </div>
-                  <div class="col-md-2">
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
-                  </div>
+              </div>
+              <div class="form-group">
+                <div class="col-md-4">
+                  <input name="firstname" type="text" class="form-control" id="" placeholder="First Name">
+                </div>
+                <div class="col-md-4">
+                  <input name="lastname" type="text" class="form-control" id="" placeholder="Last Name">
+                </div>
+                <div class="col-md-2">
+                  <button type="submit" class="btn btn-primary">Subscribe</button>
                 </div>
               </div>
             </div>
-            <hr>
-            <div class="small">&copy; 2015 SAFER Campus. All Rights Reserved.</div>
           </div>
-        </footer>
+          <hr>
+          <div class="small">&copy; 2015 SAFER Campus. All Rights Reserved.</div>
+        </div>
       </div>
+    </footer>
+
   </div>
   <? Loader::element('footer_required'); // concrete5 setup  ?>
 </body>

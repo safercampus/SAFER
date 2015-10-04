b05a3808c7a2c5237de43b1b0e5efee56e670c11
Wesley Reitzfeld <wes.reitzfeld@gmail.com>
2015-10-04 10:23:28 -0400

    Use CMS for donation form content

--- application/single_pages/donate.php
+++ application/single_pages/donate.php
@@ -19,14 +19,21 @@
       <div class="panel panel-default">
         <div class="panel-heading collapsed" role="button" data-toggle="collapse" data-target="#donationForm" aria-expanded="false" aria-controls="donationForm">
           <div class="panel-title">
-              <span class="title">Donate to SAFER</span>
-              <i class="glyphicon glyphicon-chevron-up"></i>
-              <i class="glyphicon glyphicon-chevron-down"></i>
+            <?php
+              $a = new Area('Donation Form Panel Title');
+              $a->display($c);
+            ?>
+            <i class="glyphicon glyphicon-chevron-up"></i>
+            <i class="glyphicon glyphicon-chevron-down"></i>
           </div>
         </div>
 
         <div class="panel-body collapse" id="donationForm">
           <!-- Trail Blazer 10/02/2015 -->
+          <?php
+              $a = new Area('Content Above Donation Form');
+              $a->display($c);
+          ?>
           <iframe id="tbzframe"
               src="https://www.trailblz.info/SAFER/Donations.aspx?rdif=http%3a%2f%2fsafercampus.org%2fthanks-for-donating&ver=2"
               frameborder="0"
@@ -34,10 +41,10 @@
               onload="scroll(0,0)"
               allowtransparency="true"></iframe>
           <!-- Trail Blazer 10/02/2015 -->
-          SAFER is 501(c)3 non-profit organization, and your contribution is entirely tax-deductible. Thank you for your support!
-          <br>
-          <br>
-          Checks should be sent to SAFER at 222 Broadway, 19th Floor, New York, NY 10038
+          <?php
+              $a = new Area('Content Below Donation Form');
+              $a->display($c);
+          ?>
         </div>
       </div>
     </div>
